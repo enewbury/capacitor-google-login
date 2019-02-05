cf54e51235eea02c551fad15e186bb0f6248d0c2
Eric Newbury <enewbury94@gmail.com>
2019-02-05 16:33:14 -0500

    update to js sdk

--- src/web.ts
+++ src/web.ts
@@ -1,13 +1,13 @@
 import { WebPlugin } from '@capacitor/core';
 import { GoogleLoginOptions, GoogleLoginPlugin, GoogleLoginResponse } from './definitions';
-import { WebUtils } from './web-utils';
+
+declare var window: any;
+declare var gapi: any;
 
 export class GoogleLoginWeb extends WebPlugin implements GoogleLoginPlugin {
 
-  private windowHandle: Window = null;
-  private intervalId: number = null;
-  private loopCount = 1000;
-  private intervalLength = 100;
+  private gapiLoaded: boolean;
+  private auth2: any;
 
   constructor() {
     super({
@@ -16,67 +16,38 @@ export class GoogleLoginWeb extends WebPlugin implements GoogleLoginPlugin {
     });
   }
 
+  load() {
+    window.googleAsyncInit = () => {
+      console.log('Init Google JS SDK');
+      gapi.load('auth2', () => {
+        this.gapiLoaded = true;
+      });
+    };
+
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+      let js;
+      const fjs = d.getElementsByTagName(s)[0];
+      js = d.createElement(s);
+      js.id = id;
+      (js as HTMLScriptElement).src = 'https://apis.google.com/js/client:platform.js?onload=googleAsyncInit';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'google-jssdk'));
+  }
+
   async authenticate(options: GoogleLoginOptions): Promise<GoogleLoginResponse> {
     return new Promise<any>((resolve, reject) => {
-      if (!options.web && !options.web.redirectUrl) {
-        return reject(new Error('Required "web.redirectUrl" not found!'));
+      if (!this.gapiLoaded) {
+        reject("Google Api hasn't loaded");
       }
 
-      let loopCount = this.loopCount;
+      this.auth2 = gapi.auth2.init({
+        client_id: options.serverAppId,
+        scope: options.scopes.join("+")
+      });
 
-      this.windowHandle = window.open(WebUtils.getAuthorizationUrl(options), '_blank', 'height=600,width=600,left=0,top=0');
-
-      this.intervalId = setInterval(() => {
-        if (loopCount-- < 0) {
-          this.closeWindow();
-        } else {
-          let href: string;
-          try {
-            href = this.windowHandle.location.href;
-          } catch (e) {
-            console.log(e);
-          }
-          if (href != null) {
-            const urlParamObj = WebUtils.getUrlParams(href);
-            if (urlParamObj) {
-              this.processResponse(urlParamObj, href, options, resolve, reject);
-            }
-          }
-        }
-      }, this.intervalLength);
+      this.auth2.grantOfflineAccess().then(resolve)
     });
-  }
-
-  private processResponse(urlParamObj: { state: string, code: string }, href: string, options: GoogleLoginOptions, resolve: Function, reject: Function) {
-    clearInterval(this.intervalId);
-    if (options.web.stateDisabled || urlParamObj.state === options.web.state) {
-      if (href.match(/code=(.*)/)) {
-        const code = urlParamObj.code;
-        if (code) {
-          const resp = {
-            code: code,
-          };
-          resolve(resp);
-          this.closeWindow();
-        } else {
-          reject(new Error('No code returned! Authentication failed!'));
-          this.closeWindow();
-        }
-      } else {
-        if (href.indexOf(options.web.redirectUrl) === 0) {
-          reject(new Error('Code not found in redirect url!'));
-          this.closeWindow();
-        }
-      }
-    } else {
-      reject(new Error('State check not passed! Retrieved state does not match sent one!'));
-      this.closeWindow();
-    }
-  }
-
-  private closeWindow() {
-    clearInterval(this.intervalId);
-    this.windowHandle.close();
   }
 }
 
