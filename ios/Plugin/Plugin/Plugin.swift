b6549241960642579d76ea7d006d02cdc503934b
Eric Newbury <enewbury94@gmail.com>
2019-02-05 15:47:40 -0500

    bump

--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -7,24 +7,30 @@ import GoogleSignIn
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
  */
 @objc(GoogleLogin)
-public class GoogleLogin: CAPPlugin, GIDSignInDelegate {
+public class GoogleLogin: CAPPlugin, GIDSignInDelegate, GIDSignInUIDelegate {
 
     var savedLastCall: CAPPluginCall?
 
-    public func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        if let error = error {
-            print("\(error.localizedDescription)")
-        } else {
-            let data = [ "code": user.serverAuthCode as Any ]
-            self.savedLastCall?.success(data)
-        }
-    }
-
     public override func load() {
+        print("loging")
         NotificationCenter.default.addObserver(self, selector: #selector(self.urlOpened(notification:)), name: Notification.Name(CAPNotifications.URLOpen.name()), object: nil)
 
-
         GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+    }
+
+    @objc func authenticate(_ call: CAPPluginCall) {
+        self.savedLastCall = call
+
+        let iosConfig = call.getObject("ios") ?? [:]
+        let iosAppId = iosConfig["appId"] as? String
+        GIDSignIn.sharedInstance().clientID = iosAppId
+        GIDSignIn.sharedInstance().serverClientID = call.getString("serverAppId")
+        GIDSignIn.sharedInstance().scopes = ["email", "profile"]
+        print("trying to signin")
+        GIDSignIn.sharedInstance().signOut()
+        GIDSignIn.sharedInstance().signIn()
+        print("called signin")
     }
 
     @objc func urlOpened(notification: NSNotification) {
@@ -36,16 +42,20 @@ public class GoogleLogin: CAPPlugin, GIDSignInDelegate {
                                           annotation: options[UIApplicationOpenURLOptionsKey.annotation])
     }
 
-    @objc func authenticate(_ call: CAPPluginCall) {
-        self.savedLastCall = call
+    public func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print("\(error.localizedDescription)")
+        } else {
+            let data = [ "code": user.serverAuthCode as Any ]
+            self.savedLastCall?.success(data)
+        }
+    }
 
-        let iosConfig = call.getObject("ios") ?? [:]
-        let iosAppId = iosConfig["appId"] as? String
-        GIDSignIn.sharedInstance().clientID = iosAppId
-        GIDSignIn.sharedInstance().serverClientID = call.getString("serverAppId")
-        GIDSignIn.sharedInstance().scopes = ["email", "profile"]
+    public func sign(_ signIn: GIDSignIn!, present viewController: UIViewController!) {
+        self.bridge.viewController.present(viewController, animated: true, completion: nil)
+    }
 
-        GIDSignIn.sharedInstance().signIn()
-        print("called signin")
+    public func sign(_ signIn: GIDSignIn!, dismiss viewController: UIViewController!) {
+        self.bridge.viewController.dismiss(animated: true, completion: nil)
     }
 }
